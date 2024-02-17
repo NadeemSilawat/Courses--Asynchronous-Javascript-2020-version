14a0d56a9a10a06ed5e359a46cffb03b150e63fd
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-02-17 10:37:08 +0530

    Using JSON Data

---
+++ 05-using-json-data.js
@@ -0,0 +1,29 @@
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+            callback(undefined, data);
+        } else if (request.readyState === 4) {
+            callback('coud not fetch data',undefined);
+        }
+    });
+
+
+    request.open('GET', 'using json.json');   // json file 
+
+    request.send();
+
+};
+
+
+getTodos((err, data) => {           // -- Non blocking Code  Asynchronous code 
+    console.log('callback fired')
+    if(err){
+        console.log(err);
+    }else{
+        console.log(data);
+
+    }
+});
