5eb78561d637d9e0df27bde9359446a9657954ca
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-02-18 12:20:40 +0530

    Async Await

---
+++ 10-async-await.js
@@ -0,0 +1,17 @@
+const getTodos = async () => {
+    const response = await fetch('/06-callback-hell/user1.json')
+    const data = await response.json()
+
+    return data;
+};
+
+
+console.log(1);
+console.log(2);
+
+getTodos().then((data) => {
+    console.log('resolved',data);
+});
+
+console.log(3);
+console.log(4);
